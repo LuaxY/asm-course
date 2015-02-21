145a6f31249aa04f52ef05bf23ce8de3929d65e6
LuaxY <yann.guineau@voidmx.net>
2015-02-21 21:55:15 +0100

    Implement new line

--- bioscall_print_string.asm
+++ bioscall_print_string.asm
@@ -9,17 +9,25 @@ mov cx, 1 ; char repetition
 
 display_next:
 mov al, [si] ; stock next char to display in AL register
-or al, al ; compare AL to 0 (EOL)
+or al, al ; compare current char to 0 (EOL)
 jz end_display_next ; if AL == O jump to end
+cmp al, 13 ; compare current char to 13 (new line)
+je  new_line ; jump to new line
+print_char:
 mov ah, 0x02 ; set cursor position
 int 0x10 ; call BIOS interruption
 mov ah, 0x0A ; display current char CX time
 int 0x10 ; call BIOS interruption
-inc si ; continue to next char
-inc dl ; continue to next cursor position
-jmp display_next ; while to next char
+inc si ; increment to next char
+inc dl ; increment to next cursor position
+jmp display_next ; jump to next char
 
 end_display_next:
 ret ; end en program
 
-hello: db 'Hello World!', 0
+new_line:
+inc dh ; go to next line
+xor dl, dl ; set to first column
+jmp print_char
+
+hello: db 'Hello World!', 13, 0
